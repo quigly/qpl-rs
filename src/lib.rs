6d7e835f1306bd34f341a8642120973d0bc461c5
quigly <quiglyf2p@gmail.com>
2022-10-14 01:15:14 -0400

    i hate git. i miss svn.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,58 @@
-/// i really dont like git
-pub fn eat_shit()
+#[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "windows")]
+pub use windows::*;
+
+#[cfg(target_os = "linux")]
+mod linux;
+#[cfg(target_os = "linux")]
+pub use linux::*;
+
+#[derive(Debug, Copy, Clone)]
+pub enum PowerState
 {
-    println!("Eat shit!");
+	Unknown,
+	OnBattery,
+	NoBattery,
+	Charging,
+	Charged
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct PowerInfo
+{
+	pub state: PowerState,
+	pub seconds: i32,
+	pub percent: i32
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct SystemMemory
+{
+	pub total: u64,
+	pub avail: u64
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
+pub enum WindowMode
+{
+    Windowed,
+    Borderless,
+    Fullscreen
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum Event
+{
+    Quit
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct WindowCreateInfo<'a>
+{
+    pub width: u32,
+    pub height: u32,
+    pub title: &'a str,
+    pub mode: WindowMode,
+    pub resizable: bool
 }
