d30099a7728d7f5be8bb40d84843e818c42ba802
Quigly <quigly@users.noreply.github.com>
2022-12-10 18:57:23 -0500

    implemented is_key_down

--- src/linux.rs
+++ src/linux.rs
@@ -223,6 +223,8 @@ impl Window
 				let keycode: u32 = unsafe { xevent.key.keycode };
 				let key = x11_convert_keycode_to_key(keycode);
 
+				self.keys_current[key as usize] = true;
+
 				let modifiers = KeyModifiers
 				{
 					..Default::default()
@@ -239,6 +241,8 @@ impl Window
 			{
 				let keycode: u32 = unsafe { xevent.key.keycode };
 				let key = x11_convert_keycode_to_key(keycode);
+
+				self.keys_current[key as usize] = false;
 
 				let modifiers = KeyModifiers
 				{
@@ -308,17 +312,17 @@ impl Window
 
 	pub fn is_key_down(&self, key: Key) -> bool
 	{
-		todo!()
+		self.keys_current[key as usize]
 	}
 
 	pub fn is_key_up(&self, key: Key) -> bool
 	{
-		todo!()
+		!self.keys_current[key as usize]
 	}
 
 	pub fn is_key_pressed(&self, key: Key) -> bool
 	{
-		todo!()
+		self.keys_current[key as usize] && !self.keys_previous[key as usize]
 	}
 
 	pub fn gl_create_context(&self, create_info: &GLContextCreateInfo) -> Result<GLContext, GLError>
@@ -416,7 +420,7 @@ impl Window
 			gl::DebugMessageCallback(Some(gl_debug_message_callback), std::ptr::null());
 		}
 
-		Ok(GLContext { glx_context, handle })
+		Ok(GLContext { glx_context, handle: self.handle })
 	}
 
 	pub fn vk_create_surface(&self, entry: &ash::Entry, instance: &ash::Instance, allocation_callbacks: Option<&ash::vk::AllocationCallbacks>) -> ash::vk::SurfaceKHR
